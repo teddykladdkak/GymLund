e621aaba6f804c481da753d170972e127896c269
Mattias Lidbeck <teddykladdkak@gmail.com>
2017-10-04 13:54:26 +0200

    Quiz
    Då jag inte får till Google Maps APIn så löser jag det istället med att
    lägga till embend länk i Library.

--- filer/library/gymlund.js
+++ filer/library/gymlund.js
@@ -4,733 +4,838 @@ var gyms = [{
 	location: {
 		longitud: '55.712638',
 		latitud: '13.275641'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2247.63949017107!2d13.273452315929017!3d55.71263798054264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQyJzQ1LjUiTiAxM8KwMTYnMzIuMyJF!5e0!3m2!1ssv!2sus!4v1507117434234'
 },{
 	namn: 'Square Church',
 	id: 'square-church',
 	location: {
 		longitud: '55.696045',
 		latitud: '13.234166'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.594064105796!2d13.23197731592834!3d55.69604498053831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQxJzQ1LjgiTiAxM8KwMTQnMDMuMCJF!5e0!3m2!1ssv!2sus!4v1507117511566'
 },{
 	namn: 'Gömd Helig Plats',
 	id: 'g_md-helig-plats',
 	location: {
 		longitud: '55.685989',
 		latitud: '13.1717160'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.1724810700794!2d13.16952731592795!3d55.68598898053558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQxJzA5LjYiTiAxM8KwMTAnMTguMiJF!5e0!3m2!1ssv!2sus!4v1507117557255'
 },{
 	namn: 'Sankt Lars Torn',
 	id: 'sankt-lars-torn',
 	location: {
 		longitud: '55.68722',
 		latitud: '13.186326'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.101678179965!2d13.184137315928027!3d55.68721998053584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQxJzE0LjAiTiAxM8KwMTEnMTAuOCJF!5e0!3m2!1ssv!2sus!4v1507117618048'
 },{
 	namn: 'Höjeådalen',
 	id: 'h_je_dalen',
 	location: {
 		longitud: '55.685058',
 		latitud: '13.1768580'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.226028301511!2d13.174669315927943!3d55.685057980535326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQxJzA2LjIiTiAxM8KwMTAnMzYuNyJF!5e0!3m2!1ssv!2sus!4v1507117695439'
 },{
 	namn: 'Flackarps mölla',
 	id: 'flackarps-m_lla',
 	location: {
 		longitud: '55.681885',
 		latitud: '13.1677230'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.4085215475193!2d13.165534315927763!3d55.68188498053445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQwJzU0LjgiTiAxM8KwMTAnMDMuOCJF!5e0!3m2!1ssv!2sus!4v1507117743702'
 },{
 	namn: 'Vattenfigur',
 	id: 'vattenfigur',
 	location: {
 		longitud: '55.703785',
 		latitud: '13.1843270'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.148815329868!2d13.182138315928663!3d55.703784980540306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQyJzEzLjYiTiAxM8KwMTEnMDMuNiJF!5e0!3m2!1ssv!2sus!4v1507117773560'
 },{
 	namn: 'Plaskdammen',
 	id: 'plaskdammen',
 	location: {
 		longitud: '55.693107',
 		latitud: '13.1778870'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.763063814628!2d13.17569831592825!3d55.69310698053746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQxJzM1LjIiTiAxM8KwMTAnNDAuNCJF!5e0!3m2!1ssv!2sus!4v1507117804573'
 },{
 	namn: 'Water Stone',
 	id: 'water-stone',
 	location: {
 		longitud: '55.701508',
 		latitud: '13.1936180'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.2798055666967!2d13.191429315928563!3d55.70150798053965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQyJzA1LjQiTiAxM8KwMTEnMzcuMCJF!5e0!3m2!1ssv!2sus!4v1507117846180'
 },{
 	namn: 'Wall Statue',
 	id: 'wall-statue',
 	location: {
 		longitud: '55.696456',
 		latitud: '13.1889740'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.5704220821995!2d13.186785315928358!3d55.696455980538325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQxJzQ3LjIiTiAxM8KwMTEnMjAuMyJF!5e0!3m2!1ssv!2sus!4v1507117869918'
 },{
 	namn: 'Small Fountain',
 	id: 'small-fountain',
 	location: {
 		longitud: '55.702010',
 		latitud: '13.1700640'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.250927040505!2d13.167875315928525!3d55.70200998053983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQyJzA3LjIiTiAxM8KwMTAnMTIuMiJF!5e0!3m2!1ssv!2sus!4v1507117891555'
 },{
 	namn: 'Tunnelmålning Polhem',
 	id: 'tunnelm_lning-polhem',
 	location: {
 		longitud: '55.700452',
 		latitud: '13.1772350'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.3405534568014!2d13.175046315928535!3d55.70045198053946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQyJzAxLjYiTiAxM8KwMTAnMzguMSJF!5e0!3m2!1ssv!2sus!4v1507117915775'
 },{
 	namn: 'Demona the Gargoyle',
 	id: 'demona-the-gargoyle',
 	location: {
 		longitud: '55.686655',
 		latitud: '13.1872690'
-	}
+	},
+	embed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.1341751674486!2d13.185080315927996!3d55.68665498053575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQxJzEyLjAiTiAxM8KwMTEnMTQuMiJF!5e0!3m2!1ssv!2sus!4v1507117948312'
 },{
 	namn: 'Netto Park',
 	id: 'netto-park',
 	location: {
 		longitud: '55.703840',
 		latitud: '13.1594460'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Rosa Ansiktet',
 	id: 'rosa-ansiktet',
 	location: {
 		longitud: '55.698182',
 		latitud: '13.1899020'
-	}
+	},
+	embed: ''
 },{
 	namn: 'The Spider web',
 	id: 'the-spider-web',
 	location: {
 		longitud: '55.692435',
 		latitud: '13.1974880'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Fountain',
 	id: 'fountain',
 	location: {
 		longitud: '55.699108',
 		latitud: '13.1850550'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Fontänen Vid stadsparken',
 	id: 'font_nen-vid-stadsparken',
 	location: {
 		longitud: '55.700644',
 		latitud: '13.1861260'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Utomhusscen',
 	id: 'utomhusscen',
 	location: {
 		longitud: '55.698194',
 		latitud: '13.1841300'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Smålands',
 	id: 'sm_lands',
 	location: {
 		longitud: '55.697954',
 		latitud: '13.1984760'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Petersgården',
 	id: 'petersg_rden',
 	location: {
 		longitud: '55.702455',
 		latitud: '13.1760510'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Security Man Statue',
 	id: 'security-man-statue',
 	location: {
 		longitud: '55.693159',
 		latitud: '13.1791070'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Domkyrka',
 	id: 'domkyrka',
 	location: {
 		longitud: '55.704145',
 		latitud: '13.1931110'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Tetra Pak Circle',
 	id: 'tetra-pak-circle',
 	location: {
 		longitud: '55.690127',
 		latitud: '13.1914970'
-	}
+	},
+	embed: ''
 },{
 	namn: 'UARDA Akademin',
 	id: 'uarda-akademin',
 	location: {
 		longitud: '55.705631',
 		latitud: '13.2042700'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Metal Swirl',
 	id: 'metal-swirl',
 	location: {
 		longitud: '55.709329',
 		latitud: '13.1633260'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Afrodite',
 	id: 'afrodite',
 	location: {
 		longitud: '55.709924',
 		latitud: '13.2165700'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Kosmos Budbärare',
 	id: 'kosmos-budb_rare',
 	location: {
 		longitud: '55.698746',
 		latitud: '13.2145970'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Olika Stenar',
 	id: 'olika-stenar',
 	location: {
 		longitud: '55.703243',
 		latitud: '13.2040720'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Lund Sk8 Spot',
 	id: 'lund-sk8-spot',
 	location: {
 		longitud: '55.694658',
 		latitud: '13.2184590'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Institutionen för Psykologi',
 	id: 'institutionen-f_r-psykologi',
 	location: {
 		longitud: '55.707106',
 		latitud: '13.1955710'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Sorgen',
 	id: 'sorgen',
 	location: {
 		longitud: '55.714732',
 		latitud: '13.1943720'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Märke Åke Hans',
 	id: 'm_rke-_ke-hans',
 	location: {
 		longitud: '55.706979',
 		latitud: '13.1925630'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Klätterställning',
 	id: 'kl_tterst_llning',
 	location: {
 		longitud: '55.711406',
 		latitud: '13.1675540'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Golden Arch',
 	id: 'golden-arch',
 	location: {
 		longitud: '55.713333',
 		latitud: '13.1997900'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Litet Kapell',
 	id: 'litet-kapell',
 	location: {
 		longitud: '55.712565',
 		latitud: '13.1930930'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Djur Att Leka Med',
 	id: 'djur-att-leka-med',
 	location: {
 		longitud: '55.709386',
 		latitud: '13.1828280'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Totem',
 	id: 'totem',
 	location: {
 		longitud: '55.707013',
 		latitud: '13.1911490'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Kapitalet',
 	id: 'kapitalet',
 	location: {
 		longitud: '55.709822',
 		latitud: '13.2094180'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Skorsten',
 	id: 'skorsten',
 	location: {
 		longitud: '55.713930',
 		latitud: '13.2026740'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Fontänen Bakom Stationen',
 	id: 'font_nen-bakom-stationen',
 	location: {
 		longitud: '55.705228',
 		latitud: '13.1854690'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Museum Wall Art',
 	id: 'museum-wall-art',
 	location: {
 		longitud: '55.707981',
 		latitud: '13.1983670'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Lund University Main Library',
 	id: 'lund-university-main-library',
 	location: {
 		longitud: '55.708827',
 		latitud: '13.1970750'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Lundagård Fountain',
 	id: 'lundag_rd-fountain',
 	location: {
 		longitud: '55.705644',
 		latitud: '13.1942780'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Botaniska Trädgården',
 	id: 'botaniska-tr_dg_rden',
 	location: {
 		longitud: '55.703144',
 		latitud: '13.2013650'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Giant Lion',
 	id: 'giant-lion',
 	location: {
 		longitud: '55.690295',
 		latitud: '13.2151000'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Freedom',
 	id: 'freedom',
 	location: {
 		longitud: '55.705348',
 		latitud: '13.1883550'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Environmental Engineering W',
 	id: 'environmental-engineering-w',
 	location: {
 		longitud: '55.714960',
 		latitud: '13.2095060'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Spyken',
 	id: 'spyken',
 	location: {
 		longitud: '55.701171',
 		latitud: '13.2009020'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Allhelgonakyrkan',
 	id: 'allhelgonakyrkan',
 	location: {
 		longitud: '55.709586',
 		latitud: '13.1935820'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Rinnebäcksravinen',
 	id: 'rinneb_cksravinen',
 	location: {
 		longitud: '55.708247',
 		latitud: '13.1575270'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Pencil',
 	id: 'pencil',
 	location: {
 		longitud: '55.714257',
 		latitud: '13.2116750'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Bjärredsparken Amfiteater',
 	id: 'bj_rredsparken-amfiteater',
 	location: {
 		longitud: '55.707506',
 		latitud: '13.1847240'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Saturnushuset',
 	id: 'saturnushuset',
 	location: {
 		longitud: '55.698437',
 		latitud: '13.2161590'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Öresundsvägens Information Plaque',
 	id: '_resundsv_gens-information-plaque',
 	location: {
 		longitud: '55.712290',
 		latitud: '13.1706610'
-	}
+	},
+	embed: ''
 },{
 	namn: 'EOS Hallen',
 	id: 'eos-hallen',
 	location: {
 		longitud: '55.696571',
 		latitud: '13.2012840'
-	}
+	},
+	embed: ''
 },{
 	namn: 'The Dead Fountain',
 	id: 'the-dead-fountain',
 	location: {
 		longitud: '55.711115',
 		latitud: '13.2089610'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Hallands Nations Salmon',
 	id: 'hallands-nations-salmon',
 	location: {
 		longitud: '55.705732',
 		latitud: '13.2111600'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Katt med Blomma',
 	id: 'katt-med-blomma',
 	location: {
 		longitud: '55.718639',
 		latitud: '13.1786150'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Gunnesbo Station Norrgående',
 	id: 'gunnesbo-station-norrg_ende',
 	location: {
 		longitud: '55.726534',
 		latitud: '13.1677240'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Swing It',
 	id: 'swing-it',
 	location: {
 		longitud: '55.725764',
 		latitud: '13.1719800'
-	}
+	},
+	embed: ''
 },{
 	namn: 'The Wood Throne',
 	id: 'the-wood-throne',
 	location: {
 		longitud: '55.725104',
 		latitud: '13.1935690'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Fyrklöverns Trädgård',
 	id: 'fyrkl_verns-tr_dg_rd',
 	location: {
 		longitud: '55.721568',
 		latitud: '13.2175470'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Energy Swing',
 	id: 'energy-swing',
 	location: {
 		longitud: '55.729063',
 		latitud: '13.2162800'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Forntid',
 	id: 'forntid',
 	location: {
 		longitud: '55.728790',
 		latitud: '13.2100360'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Wallmire Cross',
 	id: 'wallmire-cross',
 	location: {
 		longitud: '55.740950',
 		latitud: '13.1702360'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Northern Information Plaque',
 	id: 'northern-information-plaque',
 	location: {
 		longitud: '55.729023',
 		latitud: '13.2019200'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Victoriastadion',
 	id: 'victoriastadion',
 	location: {
 		longitud: '55.718740',
 		latitud: '13.2015290'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Vind',
 	id: 'vind',
 	location: {
 		longitud: '55.724282',
 		latitud: '13.1678860'
-	}
+	},
+	embed: ''
 },{
 	namn: 'HG Spaceship',
 	id: 'hg-spaceship',
 	location: {
 		longitud: '55.717758',
 		latitud: '13.2266210'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Inaequalis Lacus',
 	id: 'inaequalis-lacus',
 	location: {
 		longitud: '55.718144',
 		latitud: '13.2253880'
-	}
+	},
+	embed: ''
 },{
 	namn: 'The Round Playground',
 	id: 'the-round-playground',
 	location: {
 		longitud: '55.736724',
 		latitud: '13.1793940'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Yellow Horse',
 	id: 'yellow-horse',
 	location: {
 		longitud: '55.725673',
 		latitud: '13.1582750'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Intermodal Nexus',
 	id: 'intermodal-nexus',
 	location: {
 		longitud: '55.715441',
 		latitud: '13.2141920'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Framtidsvandring',
 	id: 'framtidsvandring',
 	location: {
 		longitud: '55.722334',
 		latitud: '13.2271290'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Forrest Siluette',
 	id: 'forrest-siluette',
 	location: {
 		longitud: '55.720502',
 		latitud: '13.1930660'
-	}
+	},
+	embed: ''
 },{
 	namn: 'ideon gateway',
 	id: 'ideon-gateway',
 	location: {
 		longitud: '55.717783',
 		latitud: '13.2138940'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Play Boat',
 	id: 'play-boat',
 	location: {
 		longitud: '55.721431',
 		latitud: '13.1770260'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Underground Painting - Lund',
 	id: 'underground-painting---lund',
 	location: {
 		longitud: '55.719579',
 		latitud: '13.1793830'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Narrow Slide',
 	id: 'narrow-slide',
 	location: {
 		longitud: '55.719558',
 		latitud: '13.1932950'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Horsey',
 	id: 'horsey',
 	location: {
 		longitud: '55.712595',
 		latitud: '13.2281120'
-	}
+	},
+	embed: ''
 },{
 	namn: 'X Marks the Spot',
 	id: 'x-marks-the-spot',
 	location: {
 		longitud: '55.723136',
 		latitud: '13.2126580'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Stensvamp',
 	id: 'stensvamp',
 	location: {
 		longitud: '55.727216',
 		latitud: '13.1750650'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Bird Village',
 	id: 'bird-village',
 	location: {
 		longitud: '55.724433',
 		latitud: '13.1887850'
-	}
+	},
+	embed: ''
 },{
 	namn: 'QlikView Obelisk',
 	id: 'qlikview-obelisk',
 	location: {
 		longitud: '55.717067',
 		latitud: '13.2207730'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Nöbbelöv Bridge',
 	id: 'n_bbel_v-bridge',
 	location: {
 		longitud: '55.722656',
 		latitud: '13.1715820'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Borgarparken Fountain',
 	id: 'borgarparken-fountain',
 	location: {
 		longitud: '55.723458',
 		latitud: '13.2089190'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Informationdesk Lund East',
 	id: 'informationdesk-lund-east',
 	location: {
 		longitud: '55.696359',
 		latitud: '13.2265250'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Norra Nöbelövs Kyrka',
 	id: 'norra-n_bel_vs-kyrka',
 	location: {
 		longitud: '55.732278',
 		latitud: '13.1664900'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Annehems Gård',
 	id: 'annehems-g_rd',
 	location: {
 		longitud: '55.730359',
 		latitud: '13.1852520'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Battle at Lund Monument',
 	id: 'battle-at-lund-monument',
 	location: {
 		longitud: '55.719353',
 		latitud: '13.1845240'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Top of the Hill',
 	id: 'top-of-the-hill',
 	location: {
 		longitud: '55.722105',
 		latitud: '13.1886970'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Solbjer',
 	id: 'solbjer',
 	location: {
 		longitud: '55.717716',
 		latitud: '13.2349280'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Web',
 	id: 'web',
 	location: {
 		longitud: '55.722839',
 		latitud: '13.1995230'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Rymdskeppet',
 	id: 'rymdskeppet',
 	location: {
 		longitud: '55.723224',
 		latitud: '13.2034950'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Bird Tower',
 	id: 'bird-tower',
 	location: {
 		longitud: '55.723503',
 		latitud: '13.1954830'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Accordion Kerub',
 	id: 'accordion-kerub',
 	location: {
 		longitud: '55.717500',
 		latitud: '13.1852770'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Monkey Statue',
 	id: 'monkey-statue',
 	location: {
 		longitud: '55.697213',
 		latitud: '13.2389210'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Twin Tower Hill',
 	id: 'twin-tower-hill',
 	location: {
 		longitud: '55.706053',
 		latitud: '13.2468540'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Strand På Vägg',
 	id: 'strand-p_-v_gg',
 	location: {
 		longitud: '55.748228',
 		latitud: '13.2001580'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Tree of Pacifiers',
 	id: 'tree-of-pacifiers',
 	location: {
 		longitud: '55.708007',
 		latitud: '13.2358140'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Sheet of Power',
 	id: 'sheet-of-power',
 	location: {
 		longitud: '55.701612',
 		latitud: '13.2511660'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Tre Högar',
 	id: 'tre-h_gar',
 	location: {
 		longitud: '55.699180',
 		latitud: '13.2426320'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Dangerous Tilter',
 	id: 'dangerous-tilter',
 	location: {
 		longitud: '55.693025',
 		latitud: '13.2405100'
-	}
+	},
+	embed: ''
 },{
 	namn: 'Toolgate',
 	id: 'toolgate',
 	location: {
 		longitud: '55.686175',
 		latitud: '13.2219540'
-	}
+	},
+	embed: ''
 }];
