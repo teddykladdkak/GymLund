28d20511b102075c6aa73887991d356c71a54db5
Mattias Lidbeck <teddykladdkak@gmail.com>
2017-11-13 13:23:44 +0100

    Bugg...
    - Felstavning såklart

--- filer/library/gymgbg.js
+++ filer/library/gymgbg.js
@@ -4018,7 +4018,7 @@ var gyms = [{
 	}
 },{
 	namn: 'Färjenäsparken Ingång Karl IX Väg',
-	id: 'f_rjen_sparken-inga_ng-karl_ix-v_g',
+	id: 'f_rjen_sparken-ing_ng-karl_ix-v_g',
 	location: {
 		longitud: '57.697143',
 		latitud: '11.899756'
